533f57a6d6adf1bf22381d02496d67858109daac
Chaitanya Paranandi <cparanandi@osius.com>
2024-08-02 12:20:49 +0530

    Changes to layout and Nav menu using Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./assets/css/styles.css";
-import perfil from "./assets/img/perfil.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import about_img from "./assets/img/about.jpg";
 import work1 from "./assets/img/work1.jpg";
 import work2 from "./assets/img/work2.jpg";
@@ -7,65 +8,25 @@ import work3 from "./assets/img/work3.jpg";
 import work4 from "./assets/img/work4.jpg";
 import work5 from "./assets/img/work5.jpg";
 import work6 from "./assets/img/work6.jpg";
+// import Header from './components/navigation/Header.jsx';
+import Footer from "./components/navigation/Footer.jsx";
+import Intro from "./components/molecules/Intro.jsx";
+import MaterialHeader from "./components/navigation/MaterialHeader.jsx";
 
 const App = () => {
+  AOS.init({
+    offset: 200,
+    duration: 600,
+    easing: 'ease-in-sine',
+    delay: 100,
+  });
+
   return (
     <>
-    <header className="l-header">
-            <nav className="nav bd-grid">
-                <div>
-                    <a href="#" className="nav__logo">Marlon</a>
-                </div>
-
-                <div className="nav__menu" id="nav-menu">
-                    <ul className="nav__list">
-                        <li className="nav__item"><a href="#home" className="nav__link active-link">Home</a></li>
-                        <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
-                        <li className="nav__item"><a href="#skills" className="nav__link">Skills</a></li>
-                        <li className="nav__item"><a href="#work" className="nav__link">Work</a></li>
-                        <li className="nav__item"><a href="#contact" className="nav__link">Contact</a></li>
-                    </ul>
-                </div>
-
-                <div className="nav__toggle" id="nav-toggle">
-                    <i className='bx bx-menu'></i>
-                </div>
-            </nav>
-        </header>
+      <MaterialHeader />
+      {/* <Header /> */}
       <main className="l-main">
-        <section className="home bd-grid" id="home">
-          <div className="home__data">
-            <h1 className="home__title">
-              Hi, I am <span className="home__title-color">Chaitanya2</span> Web
-              Designer
-            </h1>
-            <a href="#" className="button">
-              Contact
-            </a>
-          </div>
-          <div className="home__social">
-            <a href="" className="home__social-icon">
-              <i className="bx bxl-linkedin"></i>
-            </a>
-            <a href="" className="home__social-icon">
-              <i className="bx bxl-behance"></i>
-            </a>
-            <a href="" className="home__social-icon">
-              <i className="bx bxl-github"></i>
-            </a>
-          </div>
-          <div className="home__img">
-            <svg className="home__blob" viewBox="0 0 479 467">
-              <mask id="mask0">
-                <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
-              </mask>
-              <g mask="url(#mask0)">
-                <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
-                <image className="home__blob-img" x="50" y="60" href={perfil} />
-              </g>
-            </svg>
-          </div>
-        </section>
+        <Intro />
         <section className="about section " id="about">
           <h2 className="section-title">About</h2>
 
@@ -197,15 +158,7 @@ const App = () => {
           </div>
         </section>
       </main>
-      <footer className="footer">
-            <p className="footer__title">Marlon</p>
-            <div className="footer__social">
-                <a href="#" className="footer__icon"><i className='bx bxl-facebook' ></i></a>
-                <a href="#" className="footer__icon"><i className='bx bxl-instagram' ></i></a>
-                <a href="#" className="footer__icon"><i className='bx bxl-twitter' ></i></a>
-            </div>
-            <p className="footer__copy">&#169; Bedimcode. All rigths reserved</p>
-        </footer>
+      <Footer />
     </>
   );
 };
